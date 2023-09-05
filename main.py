4f93c83407fcda19df918343e30fe1ae8fb938f9
Oleksandr Alvarez <alexander_1@onmail.com>
2023-09-05 17:58:18 -0400

    Polishing the code: 1) dct -> birthday_this_week 2) if + \ -> if ()

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def get_birthdays_per_week(users):
     birthday_passed_dct = {}
     birthday_far_away = {}
 
-    dct = defaultdict(list)
+    dct_birthday_this_week = defaultdict(list)
 
 
     for user in users:
@@ -25,20 +25,20 @@ def get_birthdays_per_week(users):
         list_of_colleagues.append(Colleague(**user))
     
     for colleague in list_of_colleagues:
-        if today > colleague.birthday and \
-            today > datetime(today.year + 1, \
-                          colleague.birthday.month, \
-                            colleague.birthday.day).date() > week_from_now:
+        if (today > colleague.birthday and 
+            today > datetime(today.year + 1, 
+                          colleague.birthday.month, 
+                            colleague.birthday.day).date() > week_from_now):
             birthday_passed_dct[colleague.name] = colleague.birthday.strftime('%d %B')
             
-        elif today <= colleague.birthday <= week_from_now or \
-        today <= datetime(today.year + 1, \
-                          colleague.birthday.month, \
-                            colleague.birthday.day).date() <= week_from_now:
+        elif (today <= colleague.birthday <= week_from_now or 
+        today <= datetime(today.year + 1, 
+                          colleague.birthday.month, 
+                            colleague.birthday.day).date() <= week_from_now):
             if colleague.birthday.weekday() not in [5,6]:
-                dct[colleague.birthday.strftime('%A')].append(colleague.name)   
+                dct_birthday_this_week[colleague.birthday.strftime('%A')].append(colleague.name)   
             else:
-                dct['Monday'].append(colleague.name)
+                dct_birthday_this_week['Monday'].append(colleague.name)
 
         else:
             birthday_far_away[colleague.name] = colleague.birthday.strftime('%d %B')
@@ -46,21 +46,21 @@ def get_birthdays_per_week(users):
     birthday_passed_dct = dict(sorted(birthday_passed_dct.items(), key=lambda x: datetime.strptime(x[1], '%d %B'), reverse=False)) 
     birthday_far_away = dict(sorted(birthday_far_away.items(), key=lambda x: datetime.strptime(x[1], '%d %B')))
     week_names = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    dct = dict(sorted(dict(dct).items(), key=lambda x: week_names.index(x[0])))
+    dct_birthday_this_week = dict(sorted(dict(dct_birthday_this_week).items(), key=lambda x: week_names.index(x[0])))
 
 
     if len(birthday_passed_dct) == len(users):
         print("Birthdays of all colleagues have already passed")
-        print(f'These colleagues are {birthday_passed_dct}')
+        print(f'These colleagues are: {birthday_passed_dct}')
         return {}
     elif len(birthday_far_away) == len(users):
         print("This week there are no colleagues who have birthdays")
         print(f'The colleagues who have birthdays in more than a week: {birthday_far_away}')
         return {}
-    elif len(dct) > 0:
-        return dict(dct)
+    elif len(dct_birthday_this_week) > 0:
+        return dict(dct_birthday_this_week)
     else:
-        print('There are not birthdays this week')
+        print('There are no birthdays this week')
         print(f"The passed birthdays are: {birthday_passed_dct}")
         print(f"Birthdays in more than a week: {birthday_far_away}")
         return {}
